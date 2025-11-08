efc05132e830db1042b6f18232c4e8e9b0407739
Victor Alexandre Ferreira Santos <125712570+Vic7ror@users.noreply.github.com>
2025-11-08 05:22:53 -0300

    Atividade MaxMin em C#

---
+++ Pratica4/MaxMin.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica4
+{
+    class MaxMin
+    {
+
+        // Operação relevante: TESTES (if)
+        // T(n) = 2n - 2
+        public void MaxMin1(int[] vet, out int ma, out int me)
+        {
+            int cont = 0;
+            ma = me = vet[0];
+            for (int i = 1; i < vet.Length; i++)
+            {
+                cont++;
+                if (vet[i] < me)
+                    me = vet[i];
+
+                cont++;
+                if (vet[i] > ma)
+                    ma = vet[i];
+            }
+            Console.WriteLine($"MaxMin1: {cont}");
+        }
+
+        // Melhor caso: T(n) = n - 1
+        // Pior caso: T(n) = 2n - 2
+        // Caso médio: T(n) = 3n/2 - 3/2
+        public void MaxMin2(int[] vet, out int ma, out int me)
+        {
+            int cont = 0;
+            ma = me = vet[0];
+            for (int i = 1; i < vet.Length; i++)
+            {
+                cont++;
+                if (vet[i] < me)
+                    me = vet[i];
+                else
+                {
+                    cont++;
+                    if (vet[i] > ma)
+                        ma = vet[i];
+                }
+            }
+            Console.WriteLine($"MaxMin2: {cont}");
+        }
+
+        // T(n) = 3n/2 - 2
+        public void MaxMin3(int[] vet, out int ma, out int me)
+        {
+            int cont = 0;
+            if (vet[0] < vet[1])
+            {
+                cont++;
+                me = vet[0];
+                ma = vet[1];
+            }
+            else
+            {
+                cont++;
+                me = vet[1];
+                ma = vet[0];
+            }
+            for (int i = 2; i < vet.Length; i += 2)
+            {
+                cont++;
+                if (vet[i] < vet[i + 1])
+                {
+                    cont++;
+                    if (vet[i] < me)
+                    {
+                        me = vet[i];
+                        cont++;
+                    }
+                    if (vet[i + 1] > ma)
+                    {
+                        ma = vet[i + 1];
+                        cont++;
+                    } 
+                }
+                else
+                {
+                    if (vet[i + 1] < me)
+                    {
+                        me = vet[i + 1];
+                        cont++;
+                    }
+                    if (vet[i] > ma)
+                    {
+                        ma = vet[i];
+                        cont++;
+                    }
+                }
+            }
+            Console.WriteLine($"MaxMin3: {cont}");
+        }
+    }
+}
---
+++ Pratica4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cre = new int[1000];
+            int[] dec = new int[1000];
+            int[] alt = new int[1000];
+            int ma = 0;
+            int me = 0;
+            Random r = new Random();
+
+            for (int i = 0; i < cre.Length; i++)
+            {
+                cre[i] = i + 1;
+                dec[i] = 1000 - i;
+                alt[i] = r.Next(0,100);
+            }
+
+            MaxMin mxm = new MaxMin();
+            Console.WriteLine("Vetor Crescente:");
+            mxm.MaxMin1(cre, out ma, out me);
+            mxm.MaxMin2(cre, out ma, out me);
+            mxm.MaxMin3(cre, out ma, out me);
+            Console.WriteLine();
+            Console.WriteLine("Vetor Decrescente:");
+            mxm.MaxMin1(dec, out ma, out me);
+            mxm.MaxMin2(dec, out ma, out me);
+            mxm.MaxMin3(dec, out ma, out me);
+            Console.WriteLine();
+            Console.WriteLine("Vetor Aleatório:");
+            mxm.MaxMin1(alt, out ma, out me);
+            mxm.MaxMin2(alt, out ma, out me);
+            mxm.MaxMin3(alt, out ma, out me);
+
+
+        }
+    }
+}
