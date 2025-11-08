0a926b243a8a23ff945f9d69d6cf52763ceb1eb9
Victor Alexandre Ferreira Santos <125712570+Vic7ror@users.noreply.github.com>
2025-11-08 05:27:05 -0300

    Update

--- Pratica4/MaxMin.cs
+++ Pratica4/MaxMin.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pratica4
 {
     class MaxMin
     {
-
         // Operação relevante: TESTES (if)
         // T(n) = 2n - 2
         public void MaxMin1(int[] vet, out int ma, out int me)
         {
+            if (vet == null) throw new ArgumentNullException(nameof(vet));
+            if (vet.Length == 0) throw new ArgumentException("O vetor deve conter pelo menos um elemento.", nameof(vet));
+
             int cont = 0;
             ma = me = vet[0];
             for (int i = 1; i < vet.Length; i++)
@@ -33,6 +31,9 @@ namespace Pratica4
         // Caso médio: T(n) = 3n/2 - 3/2
         public void MaxMin2(int[] vet, out int ma, out int me)
         {
+            if (vet == null) throw new ArgumentNullException(nameof(vet));
+            if (vet.Length == 0) throw new ArgumentException("O vetor deve conter pelo menos um elemento.", nameof(vet));
+
             int cont = 0;
             ma = me = vet[0];
             for (int i = 1; i < vet.Length; i++)
@@ -50,23 +51,36 @@ namespace Pratica4
             Console.WriteLine($"MaxMin2: {cont}");
         }
 
-        // T(n) = 3n/2 - 2
+        // T(n) = 3n/2 - 2 (assumindo n par)
         public void MaxMin3(int[] vet, out int ma, out int me)
         {
+            if (vet == null) throw new ArgumentNullException(nameof(vet));
+            if (vet.Length == 0) throw new ArgumentException("O vetor deve conter pelo menos um elemento.", nameof(vet));
+
             int cont = 0;
+
+            if (vet.Length == 1)
+            {
+                ma = me = vet[0];
+                Console.WriteLine($"MaxMin3: {cont}");
+                return;
+            }
+
+            // inicialização com os dois primeiros
+            cont++;
             if (vet[0] < vet[1])
             {
-                cont++;
                 me = vet[0];
                 ma = vet[1];
             }
             else
             {
-                cont++;
                 me = vet[1];
                 ma = vet[0];
             }
-            for (int i = 2; i < vet.Length; i += 2)
+
+            int i;
+            for (i = 2; i + 1 < vet.Length; i += 2)
             {
                 cont++;
                 if (vet[i] < vet[i + 1])
@@ -81,10 +95,11 @@ namespace Pratica4
                     {
                         ma = vet[i + 1];
                         cont++;
-                    } 
+                    }
                 }
                 else
                 {
+                    cont++;
                     if (vet[i + 1] < me)
                     {
                         me = vet[i + 1];
@@ -97,6 +112,16 @@ namespace Pratica4
                     }
                 }
             }
+
+            // se n for ímpar, tratar último elemento isoladamente
+            if (i < vet.Length)
+            {
+                cont++;
+                if (vet[i] < me) me = vet[i];
+                cont++;
+                if (vet[i] > ma) ma = vet[i];
+            }
+
             Console.WriteLine($"MaxMin3: {cont}");
         }
     }
